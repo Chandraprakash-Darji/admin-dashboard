a99949b1314efcc97989c0df44be2f3ca2864334
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-08-26 21:47:34 +0530

    added routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,27 @@
-import { Box, StyledEngineProvider, useTheme } from "@mui/material";
-import Navbar from "./components/Navbar";
+import { Login } from "@mui/icons-material";
+import { useTheme } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+import { Dashboard, Register } from "./pages";
 
 function App() {
     const theme = useTheme();
     return (
-        <StyledEngineProvider injectFirst>
-            <Box
-                className="min-h-screen"
-                sx={{ bgcolor: theme.palette.background.default }}
-            >
-                <Navbar />
-                {/* <Player /> */}
-            </Box>
-        </StyledEngineProvider>
+        <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route path="student/:id" element={<div>Student :id</div>} />
+            <Route
+                path="student/:id/portfolio"
+                element={<div>Portfolio</div>}
+            />
+            <Route path="student/:id/record" element={<div>records</div>} />
+            <Route path="student/:id/post" element={<div>Post</div>} />
+            <Route
+                path="student/:id/acadmenics"
+                element={<div>Acadmeics</div>}
+            />
+        </Routes>
     );
 }
 
