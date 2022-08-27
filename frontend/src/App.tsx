a9330e650096fc253d1416cfb79442969c4a720e
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-08-27 19:37:52 +0530

    added global style

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,33 @@
-import { Login } from "@mui/icons-material";
-import { useTheme } from "@mui/material";
+import { GlobalStyles, useTheme } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
-import { Dashboard, Register } from "./pages";
+import { Dashboard, Login, Register } from "./pages";
 
 function App() {
     const theme = useTheme();
     return (
-        <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="login" element={<Login />} />
-            <Route path="register" element={<Register />} />
-            <Route path="student/:id" element={<div>Student :id</div>} />
-            <Route
-                path="student/:id/portfolio"
-                element={<div>Portfolio</div>}
+        <>
+            <GlobalStyles
+                styles={{
+                    body: { backgroundColor: theme.palette.background.default },
+                }}
             />
-            <Route path="student/:id/record" element={<div>records</div>} />
-            <Route path="student/:id/post" element={<div>Post</div>} />
-            <Route
-                path="student/:id/acadmenics"
-                element={<div>Acadmeics</div>}
-            />
-        </Routes>
+            <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="login" element={<Login />} />
+                <Route path="register" element={<Register />} />
+                <Route path="student/:id" element={<div>Student :id</div>} />
+                <Route
+                    path="student/:id/portfolio"
+                    element={<div>Portfolio</div>}
+                />
+                <Route path="student/:id/record" element={<div>records</div>} />
+                <Route path="student/:id/post" element={<div>Post</div>} />
+                <Route
+                    path="student/:id/acadmenics"
+                    element={<div>Acadmeics</div>}
+                />
+            </Routes>
+        </>
     );
 }
 
